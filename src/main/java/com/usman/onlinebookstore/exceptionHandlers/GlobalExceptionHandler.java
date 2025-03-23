b08642baa562c185de56b2f2ddb56d36496a2a4f
Usman Tijani <usman1607@yahoo.com>
2025-03-23 21:32:50 +0100

    handle-exception

---
+++ src/main/java/com/usman/onlinebookstore/exceptionHandlers/GlobalExceptionHandler.java
@@ -0,0 +1,18 @@
+package com.usman.onlinebookstore.exceptionHandlers;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+    @ExceptionHandler(RuntimeException.class)
+    public ResponseEntity<Map<String, String>> handleRuntimeException(RuntimeException ex) {
+        Map<String, String> errorResponse = new HashMap<>();
+        errorResponse.put("error", ex.getMessage());
+        return ResponseEntity.badRequest().body(errorResponse);
+    }
+}
